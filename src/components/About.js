3c3779f85c2c6dc6dd2441c812908894721ebc20
FlorSing <fpsingcak@gmail.com>
2023-03-13 09:38:25 +0000

    updated home page

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,22 @@
 // import React, { Component } from "react";
 import Header from './Header';
 import './about.css';
-// import photo from '../assets/FpsIDbw.jpg';
-
+import Container from "./Container";
+import Row from "./Row";
+import Col from "./Col";
+import Card from "./Card";
+import Footer from "./Footer";
+import Wrapper from "./Wrapper";
 
 const photo = require('../assets/FpsIDbw.jpg');
 
 function About() {
   
     return (
+      <Wrapper>
       <Header>
       <div class="jumbotron"      >
-
         <h1 id='name'>Florante P. Singcak</h1>
-        {/* <img class='img'></img> */}
         <h3>Front-end Web Developer</h3>
         <p>
         Highly competent with a vast wealth of transferrable professional skills gained from previous roles ready to bring on board and add value to the organisation. 
@@ -23,40 +26,29 @@ function About() {
   dedicate a full-time career for a well-established company
         </p>
       </div>
-      <div>
-      <div class="IDcard" >
-        <img src={photo} class="card-img-top" alt="photoID"></img>
+      </Header>
+      <Container>
+        <Row>
+          <Card class="IDcard" size = 'sm-4'>
+          <div class="IDcard" size>
+            {/* <img src={photo} class="card-img-top" alt="photoID"></img> */}
    
       </div>
 
-        <p>
-        Highly competent with a vast wealth of transferrable professional skills gained from previous roles ready to bring on board and add value to the organisation. 
-        </p>
-        <p>
-        Currently upgrading skills to keep up with relevant technologies and seek the right opportunity to 
-  dedicate a full-time career for a well-established company
-        </p>
-      </div>
-    
+          </Card>
+        </Row>
+      </Container>
+
+      <Footer>
       <footer>
          <span>
            Copyright: FPS 2023
          </span>
        </footer>
-      
-      </Header>
+       </Footer>
+      </Wrapper>
 
 
-
-// <Footer>
-      //     <div>
-      // <footer>
-      //   <span>
-      //     Copyright: FPS 2023
-      //   </span>
-      // </footer>
-      // </div>
-      // </Footer>
     );
 }
 
