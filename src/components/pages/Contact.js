d5bb70d31f9645420a55b1c4e8feebe691866cad
FlorSing <fpsingcak@gmail.com>
2023-03-14 11:48:13 +0000

    need to display project images

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,24 +4,38 @@ import Learn from '../Learn';
 import React, { Component } from "react";
 import Wrapper from '../Wrapper';
 import './contact.css';
-
+import Container from "../Container";
+import Row from "../Row";
+import Col from "../Col";
 
 
 class Contact extends Component {
   render() {
   return (
     <Wrapper>
+      {/* <Container>
+        <Row>
+          <Col>
+          </Col>
+        </Row>
+      </Container> */}
     <div className='contacts'>
       <h1>Contact Page</h1>
+      <p>Email: <br></br>
+      <a href='mailto: fpsingcak@gmail.com' class="btn btn-primary">fpsingcak@gmail.com</a>
+      </p> 
       <p>
-      Email: fpsingcak@gmail.com 
-
-      GitHub: https://github.com/FlorSing 
-
-      LinkedIn: https://www.linkedin.com/in/florante-singcak/ 
- 
+      GitHub: <br></br>
+      <a href='https://github.com/FlorSing' class="btn btn-primary">https://github.com/FlorSing</a> 
       </p>
-      <Link to="learn" role="button" className="btn btn-link">
+      <p>
+      LinkedIn: <br></br>
+      <a href='https://www.linkedin.com/in/florante-singcak/' class="btn btn-primary">https://www.linkedin.com/in/florante-singcak/</a> 
+      </p>
+      <p>See my CV: <br></br>
+      <a href="../../assets/2023WebDevCVfps.pdf" class="btn btn-primary">FPScv2023</a>
+      </p>
+      {/* <Link to="learn" role="button" className="btn btn-link">
         Learn More
       </Link>
       <Link to="contact" role="button" className="btn btn-link">
@@ -29,7 +43,7 @@ class Contact extends Component {
       </Link>
       <Routes>
         <Route path="learn" element={<Learn />} />
-      </Routes>
+      </Routes> */}
     </div>
 
     </Wrapper>
