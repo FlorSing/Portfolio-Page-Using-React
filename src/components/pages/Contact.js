62d94cd692509ce1f5559759157f60e4d1981872
FlorSing <fpsingcak@gmail.com>
2023-04-05 11:16:23 +0100

    updated cv and contact page layout

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,7 @@
-// import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-import Learn from '../Learn';
+
 import React, { Component } from "react";
 import Wrapper from '../Wrapper';
 import './contact.css';
-import Container from "../Container";
-import Row from "../Row";
-import Col from "../Col";
 
 
 
@@ -14,30 +9,33 @@ class Contact extends Component {
   render() {
   return (
     <Wrapper>
-      {/* <Container>
-        <Row>
-          <Col>
-          </Col>
-        </Row>
-      </Container> */}
+ 
     <div className='contacts'>
+      <div>
       <h1>Contact Page</h1>
-      <p>Email: <br></br>
+      </div>
+      <div>
+        <p>Email: <br></br>
       <a href='mailto: fpsingcak@gmail.com' class="btn btn-primary">fpsingcak@gmail.com</a>
       </p> 
-      <p>
+      </div>
+      <div>
+        <p>
       GitHub: <br></br>
       <a href='https://github.com/FlorSing' class="btn btn-primary">https://github.com/FlorSing</a> 
       </p>
-      <p>
+      </div> 
+      <div><p>
       LinkedIn: <br></br>
       <a href='https://www.linkedin.com/in/florante-singcak/' class="btn btn-primary">https://www.linkedin.com/in/florante-singcak/</a> 
       </p>
-      <p>See my CV: <br></br>
+      </div>
+      <div><p>See my CV: <br></br>
       {/* <a href="../../assets/2023WebDevCVfps.pdf" class="btn btn-primary">FPScv2023</a> */}
-      <a href='../../assets/2023WebDevCVfps.pdf' class="btn btn-primary">FPScv2023</a>
+      <a href='https://github.com/FlorSing/portfolio-page-using-react/blob/main/src/assets/FPS-cv-2023i.pdf' class="btn btn-primary">FPS-CV</a>
       </p>
-      {/* <Link to="learn" role="button" className="btn btn-link">
+      </div>
+            {/* <Link to="learn" role="button" className="btn btn-link">
         Learn More
       </Link>
       <Link to="contact" role="button" className="btn btn-link">
@@ -53,29 +51,5 @@ class Contact extends Component {
   }
 
 }
-{/* function Contact(props) {
-  return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-      Email: {fpsingcak@gmail.com} 
-
-      GitHub: {https://github.com/FlorSing} 
-
-      LinkedIn: {https://www.linkedin.com/in/florante-singcak/} 
- 
-      </p>
-      <Link to="learn" role="button" className="btn btn-link">
-        Learn More
-      </Link>
-      <Link to="contact" role="button" className="btn btn-link">
-        Learn Less
-      </Link>
-      <Routes>
-        <Route path="learn" element={<Learn />} />
-      </Routes>
-    </div>
-  );
-} */}
 
 export default Contact;
